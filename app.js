03dbf4c6915b2db854e2a719b8df4071f1fa3ef8
kabedont <jjenniferjosephina@outlook.com>
2026-06-24 15:09:30 +0800

    fix: use env port
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({extended: true}));
 app.use("/", indexRouter);
 app.set("view engine", "ejs");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 })
